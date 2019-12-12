cb1682364707ee625d2168298d490facc1e2e189
KashNJ21 <56239162+KashNJ21@users.noreply.github.com>
2019-12-11 22:41:41 -0500

    Task: added managed

--- blog_api/src/models/UserModel.py
+++ blog_api/src/models/UserModel.py
@@ -1,32 +1,27 @@
-# src/models/UserModel.py
-from marshmallow import fields, Schema
-import datetime
+# src/models/Blogpost.py
 from . import db
-from ..app import bcrypt
+import datetime
+from marshmallow import fields, Schema
 
-class UserModel(db.Model):
+
+class BlogpostModel(db.Model):
     """
-    User Model
+    Blogpost Model
     """
 
-    # table name
-    __tablename__ = 'users'
+    __tablename__ = 'blogposts'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=True)
+    title = db.Column(db.String(128), nullable=False)
+    contents = db.Column(db.Text, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     created_at = db.Column(db.DateTime)
     modified_at = db.Column(db.DateTime)
 
-    # class constructor
     def __init__(self, data):
-        """
-        Class constructor
-        """
-        self.name = data.get('name')
-        self.email = data.get('email')
-        self.password = self.__generate_hash(data.get('password'))
+        self.title = data.get('title')
+        self.contents = data.get('contents')
+        self.owner_id = data.get('owner_id')
         self.created_at = datetime.datetime.utcnow()
         self.modified_at = datetime.datetime.utcnow()
 
@@ -35,30 +30,34 @@ class UserModel(db.Model):
         db.session.commit()
 
     def update(self, data):
-    for key, item in data.items():
-      if key == 'password':
-        self.password = self.__generate_hash(value)
-      setattr(self, key, item)
-    self.modified_at = datetime.datetime.utcnow()
-    db.session.commit()
+        for key, item in data.items():
+            setattr(self, key, item)
+        self.modified_at = datetime.datetime.utcnow()
+        db.session.commit()
 
     def delete(self):
         db.session.delete(self)
         db.session.commit()
 
     @staticmethod
-    def get_all_users():
-        return UserModel.query.all()
+    def get_all_blogposts():
+        return BlogpostModel.query.all()
 
     @staticmethod
-    def get_one_user(id):
-        return UserModel.query.get(id)
+    def get_one_blogpost(id):
+        return BlogpostModel.query.get(id)
 
-    def __generate_hash(self, password):
-        return bcrypt.generate_password_hash(password, rounds=10).decode("utf-8")
-
-    def check_hash(self, password):
-        return bcrypt.check_password_hash(self.password, password)
-
-    def __repr(self):
+    def __repr__(self):
         return '<id {}>'.format(self.id)
+
+
+class BlogpostSchema(Schema):
+    """
+    Blogpost Schema
+    """
+    id = fields.Int(dump_only=True)
+    title = fields.Str(required=True)
+    contents = fields.Str(required=True)
+    owner_id = fields.Int(required=True)
+    created_at = fields.DateTime(dump_only=True)
+    modified_at = fields.DateTime(dump_only=True)
