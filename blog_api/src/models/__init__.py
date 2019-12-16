bd6e66ed94ad5b5f78246709a8d747a2a7059fd5
KashNJ21 <56239162+KashNJ21@users.noreply.github.com>
2019-12-15 20:54:32 -0500

    Task: Updated code

--- blog_api/src/models/__init__.py
+++ blog_api/src/models/__init__.py
@@ -1,12 +1,11 @@
 #src/models/__init__.py
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 # initialize our db
 db = SQLAlchemy()
-
-from flask_bcrypt import Bcrypt
-#######
-# existing code remains #
-#######
 bcrypt = Bcrypt()
+
+from .BlogpostModel import BlogpostModel, BlogpostSchema
+from .UserModel import UserModel, UserSchema
